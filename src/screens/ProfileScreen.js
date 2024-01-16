a5c474981e63ac675348e2d326876c547e48d090
jyotiprajapti <jyotiprajapati2039@gmail.com>
2024-01-16 17:47:16 +0530

    pdf permission code

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,71 +1,79 @@
-import React from 'react';
-import { View, Text, Button, Image, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, ImageBackground, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 import RNFS from 'react-native-fs';
+import { request, PERMISSIONS } from 'react-native-permissions';
 import { horizontalScale, moderateScale, verticalScale } from '../utilities/styles';
 
 const ProfileScreen = ({ route, navigation }) => {
   const { user } = route.params;
   const users = useSelector((state) => state.users);
-  console.log("users ",users)
+  const [pdfPermission, setPdfPermission] = useState(null);
 
+ 
   const selectedUser = users.find((u) => u.login.uuid === user.login.uuid);
 
+  const checkPdfPermission = async () => {
+    const permissionStatus = await request(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE);
+    setPdfPermission(permissionStatus);
+  };
+
   const generatePDF = async () => {
-    const pdfContent = `
-      User Profile:
-      Name: ${selectedUser.name.first} ${selectedUser.name.last}
-      Email: ${selectedUser.email}
-      Country: ${selectedUser.location.country}
-      Registration Date: ${new Date(selectedUser.registered.date).toDateString()}
-    `;
+    if (pdfPermission === 'granted' || pdfPermission === 'limited') {
+      const pdfContent = `
+        User Profile:
+        Name: ${selectedUser.name.first} ${selectedUser.name.last}
+        Email: ${selectedUser.email}
+        Country: ${selectedUser.location.country}
+        Registration Date: ${new Date(selectedUser.registered.date).toDateString()}
+      `;
 
-    const path = `${RNFS.DocumentDirectoryPath}/UserProfile.pdf`;
+      const path = `${RNFS.DocumentDirectoryPath}/UserProfile.pdf`;
 
-    try {
-      await RNFS.writeFile(path, pdfContent, 'utf8');
-      console.log('PDF file saved:', path);
-    } catch (error) {
-      console.error('Error saving PDF:', error);
+      try {
+        await RNFS.writeFile(path, pdfContent, 'utf8');
+        console.log('PDF file saved:', path);
+      } catch (error) {
+        console.error('Error saving PDF:', error);
+      }
+    } else {
+      // Handle if permission is not granted
+      checkPdfPermission();
+      console.warn('Write external storage permission is not granted');
     }
   };
 
   return (
-    <View style={{ flex: 1 , backgroundColor: 'white'}}>
-      <View>
-        <ImageBackground style={styles.image} source={{
-          uri:  selectedUser.picture.large,
-        }}><Text style={styles.age} >{selectedUser.registered.age}</Text></ImageBackground>
-      </View>
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <ImageBackground style={styles.image} source={{ uri: selectedUser.picture.large }}>
+        <Text style={styles.age}>{selectedUser.registered.age}</Text>
+      </ImageBackground>
       <View style={styles.view}>
-        <Text style={styles.text} >{`${selectedUser.name.first} ${selectedUser.name.last}`}</Text>
-        <Text style={styles.text} >{`Email: ${selectedUser.email}`}</Text>
-        <Text style={styles.text} >{`Email: ${selectedUser.email}`}</Text>
+        <Text style={styles.text}>{`${selectedUser.name.first} ${selectedUser.name.last}`}</Text>
+        <Text style={styles.text}>{`Email: ${selectedUser.email}`}</Text>
         <Text style={styles.text}>{`Country: ${selectedUser.location.country}`}</Text>
         <Text style={styles.text}>{`Registration Date: ${new Date(selectedUser.registered.date).toDateString()}`}</Text>
-
         <Button title="Download PDF" onPress={generatePDF} />
       </View>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
-  image:{
+  image: {
     marginTop: horizontalScale(20),
     height: horizontalScale(150),
     width: verticalScale(150),
     alignSelf: 'center',
-   
   },
-  view:{
-margin: moderateScale(20),
-
+  view: {
+    margin: moderateScale(20),
   },
-  text:{
+  text: {
     fontSize: moderateScale(20),
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
-  age:{
+  age: {
     height: horizontalScale(40),
     width: verticalScale(40),
     backgroundColor: 'pink',
@@ -74,7 +82,7 @@ margin: moderateScale(20),
     position: 'absolute',
     right: 0,
     bottom: 0,
-   }
-})
+  },
+});
 
 export default ProfileScreen;
